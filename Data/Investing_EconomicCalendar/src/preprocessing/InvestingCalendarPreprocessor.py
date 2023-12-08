ac347dd9eac758d107b9abfec93ec99ee75bb545
Lukasz Harazin <lukaszharazin@gmail.com>
2023-12-08 12:19:33 +0100

    cataloged and cleaned indicators from economic calendar

--- Data/Investing_EconomicCalendar/src/preprocessing/InvestingCalendarPreprocessor.py
+++ Data/Investing_EconomicCalendar/src/preprocessing/InvestingCalendarPreprocessor.py
@@ -1,4 +1,4 @@
-from src.SqlAlquemyInsertHandler import SqlAlquemyInsertHandler
+from src.preprocessing.SqlAlquemySelectHandler import SqlAlquemySelectHandler
 import pandas as pd
 
 from dotenv import load_dotenv
@@ -9,18 +9,24 @@ class InvestingCalendarPreprocessor:
 
     def __init__(self):
         self.all_countries = [
-            'United States', 'Japan', 'United Kingdom', 'Canada',
-            'France', 'Switzerland', 'Germany', 'Australia',
-            'Netherlands', 'Denmark', 'Sweden', 'Spain',
-            'Hong Kong', 'Italy', 'Singapore', 'Finland',
-            'Belgium', 'Norway', 'Israel', 'Ireland',
+            'United States', 'Japan', 'United Kingdom', 'Canada', 'France',
+            'Switzerland', 'Germany', 'Australia', 'Netherlands', 'Denmark',
+            'Sweden', 'Spain', 'Hong Kong', 'Italy', 'Singapore',
+            'Finland', 'Belgium', 'Norway', 'Israel', 'Ireland',
             'New Zealand', 'Austria', 'Portugal', 'Euro Zone',
             'China', 'Taiwan', 'India', 'South Korea', 'Brazil',
             'Saudi Arabia', 'South Africa', 'Mexico', 'Thailand',
-            'Indonesia',   'Malaysia', 'United Arab Emirates',
-            'Qatar', 'Kuwait', 'Türkiye',  'Philippines',
-            'Poland', 'Chile', 'Greece', 'Peru', 'Hungary',
-            'Czech Republic', 'Egypt', 'Colombia', 'Argentina', 'Russia']
+            'Indonesia', 'Malaysia', 'United Arab Emirates', 'Qatar',
+            'Kuwait', 'Türkiye', 'Philippines', 'Poland', 'Chile',
+            'Greece', 'Peru', 'Hungary', 'Czech Republic', 'Egypt',
+            'Colombia', 'Argentina', 'Russia']
+        self.selected_countries = [
+            'United States', 'Japan', 'United Kingdom', 'Canada', 'France',
+            'Switzerland', 'Germany', 'Australia', 'Netherlands', 'Sweden',
+            'Spain', 'Italy', 'Belgium', 'Norway', 'Israel', 'Ireland',
+            'Austria', 'China', 'Taiwan', 'India', 'South Korea', 'Brazil',
+            'Saudi Arabia', 'South Africa', 'Mexico', 'Indonesia',
+            'Türkiye',  'Poland', 'Argentina', 'Russia']
         self.pmi_prefixes = [
             'S&P Global Hong Kong ',
             'S&P Global Canada ',
@@ -30,6 +36,8 @@ class InvestingCalendarPreprocessor:
             'S&P Global Mexico ',
             'S&P Global India ',
             'S&P Global South Korea ',
+            'S&P Global Philippines ',
+            'S&P Global Greece ',
             'HCOB Germany ',
             'HCOB Eurozone ',
             'S&P Global ',
@@ -47,7 +55,8 @@ class InvestingCalendarPreprocessor:
             'HCOB Italy ',
             'Taiwan ',
             'Riyad Bank Saudi Arabia ',
-            'Chinese '
+            'Chinese ',
+            'Istanbul Chamber of Industry Turkey '
         ]
         self.standard_prefixes = [
             'Austrian ',
@@ -60,11 +69,19 @@ class InvestingCalendarPreprocessor:
             'National ',
             'Irish ',
             'Belgium ',
+            'Belgian ',
+            'Portuguese ',
+            'Finnish ',
             'Turkish ',
             'Brazilian ',
+            'Greek ',
+            'Philippines ',
             'Electronic Card ',
             'Monthly ',
-            'Quarterly '
+            'Quarterly ',
+            'CB ',
+            'FGV ',
+            'GfK '
         ]
         self.quarterly_suffixes = [' (Q1)', ' (Q2)', ' (Q3)', ' (Q4)']
         self.monthly_suffixes = [' (Jan)', ' (Feb)', ' (Mar)', ' (Apr)',
@@ -72,7 +89,7 @@ class InvestingCalendarPreprocessor:
                                  ' (Sep)', ' (Oct)', ' (Nov)', ' (Dec)']
 
     def explore_indicators(self, year, thres):
-        sql_handler = SqlAlquemyInsertHandler()
+        sql_handler = SqlAlquemySelectHandler()
         df = sql_handler.read_all_indicators(year)
 
         df_processed = df.loc[:, ['ReportDateTime', 'Country', 'Indicator']]
@@ -94,21 +111,22 @@ class InvestingCalendarPreprocessor:
                       ',', countries)
 
     def get_count_by_country_and_month(self):
-        sql_handler = SqlAlquemyInsertHandler()
+        sql_handler = SqlAlquemySelectHandler()
         df = sql_handler.read_count_by_country()
         df.index = df['MonthStart']
         df.index.name = None
 
-        period = pd.period_range('1999-01-01', '2023-11-01', freq='M')
+        period = pd.period_range('1999-01-01', '2023-12-01', freq='M')
         df_summary = pd.DataFrame(index=period.to_timestamp(),
                                   columns=self.all_countries)
         for country in self.all_countries:
             df_summary[country] = df[df['Country'] == country]['Count']
 
+        df_summary = df_summary.fillna(0).astype(int)
         return df_summary
 
     def process_quaterly_indicator(self, indicator, alt_indicator):
-        sql_handler = SqlAlquemyInsertHandler()
+        sql_handler = SqlAlquemySelectHandler()
         df = sql_handler.read_indicator(indicator, alt_indicator)
         df_processed = df.loc[:, ['ReportDateTime', 'Country', 'Indicator']]
 
@@ -119,14 +137,14 @@ class InvestingCalendarPreprocessor:
 
         periods = pd.PeriodIndex(df_processed['ReportDateTime'], freq='Q')-1
         df_processed['Period'] = periods.to_timestamp()
-        df_processed['Value'] = df['Actual'].str.rstrip('%').astype(float)/100
+        df_processed['Value'] = df['Actual'].str.rstrip('%').astype(float)
         df_processed = df_processed[df_processed['Indicator'] == indicator]
 
-        self.print_summary(df_processed)
+        self.print_summary(df_processed, freq='Q')
         return df_processed
 
     def process_pmi_indicator(self, indicator, alt_indicator=''):
-        sql_handler = SqlAlquemyInsertHandler()
+        sql_handler = SqlAlquemySelectHandler()
         df = sql_handler.read_indicator(indicator, alt_indicator)
         df_processed = df.loc[:, ['ReportDateTime', 'Country', 'Indicator']]
 
@@ -148,12 +166,12 @@ class InvestingCalendarPreprocessor:
         df_processed['Value'] = df['Actual'].astype(float)
         df_processed = df_processed[df_processed['Indicator'] == indicator]
 
-        self.print_summary(df_processed)
+        self.print_summary(df_processed, freq='M')
         return df_processed
 
     def process_monthly_indicator(self, indicator, alt_indicator='',
                                   pct_value=True):
-        sql_handler = SqlAlquemyInsertHandler()
+        sql_handler = SqlAlquemySelectHandler()
         df = sql_handler.read_indicator(indicator, alt_indicator)
         df_processed = df.loc[:, ['ReportDateTime', 'Country', 'Indicator']]
 
@@ -167,18 +185,18 @@ class InvestingCalendarPreprocessor:
 
         if pct_value:
             df_processed['Value'] = df['Actual'].str.rstrip('%').str.replace(
-                ',', '').astype(float)/100
+                ',', '').astype(float)
         else:
             df_processed['Value'] = df['Actual'].str.replace(',', '').apply(
                 self.money_to_int)
 
         df_processed = df_processed[df_processed['Indicator'] == indicator]
 
-        self.print_summary(df_processed)
+        self.print_summary(df_processed, freq='M')
         return df_processed
 
     def transform_to_countries_df(self, df_processed, freq):
-        period = pd.period_range('1999-01-01', '2023-11-01', freq=freq)
+        period = pd.period_range('1999-01-01', '2023-12-31', freq=freq)
         period = period.to_timestamp()
 
         df_processed.index = df_processed['Period']
@@ -200,8 +218,8 @@ class InvestingCalendarPreprocessor:
             df_processed['Indicator'] = df_processed[
                 'Indicator'].str.removesuffix(suffix)
 
-        df_processed['Indicator'].replace(
-            alt_indicator, indicator, inplace=True)
+        df_processed['Indicator'].replace(alt_indicator, indicator,
+                                          inplace=True)
 
     def money_to_int(self, str_value):
         try:
@@ -214,12 +232,14 @@ class InvestingCalendarPreprocessor:
                 power = 9
             elif str_value[-1] == 'T':
                 power = 12
+            elif str_value[:-1] == '':
+                return 0
 
             return float(str_value[:-1]) * 10**power
         except TypeError:
             return str_value
 
-    def print_summary(self, df_processed):
+    def print_summary(self, df_processed, freq):
         missing_countries = [x for x in self.all_countries
                              if x not in df_processed['Country'].unique()]
         print('Missing countries')
@@ -227,3 +247,16 @@ class InvestingCalendarPreprocessor:
 
         print('Avg publish delay: ', (df_processed['ReportDateTime'] -
                                       df_processed['Period']).mean())
+
+        df_countries = self.transform_to_countries_df(
+            df_processed, freq=freq)
+        nas = df_countries.isna().sum().sum()
+        total = df_countries.shape[0]*df_countries.shape[1]
+        coverage_25y = round((total - nas)/total*100, 1)
+        print('Data coverage since 1999:', coverage_25y, '%')
+
+        df_recent = df_countries['2013':][self.selected_countries]
+        nas = df_recent.isna().sum().sum()
+        total = df_recent.shape[0]*df_recent.shape[1]
+        coverage_11y = round((total - nas)/total*100, 1)
+        print('Data coverage since 2013 (30 countries):', coverage_11y, '%')
