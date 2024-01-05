bce6a4c7a0332bf0bbb8ed89f709b1e3565156c5
Lukasz Harazin <lukaszharazin@gmail.com>
2024-01-05 19:34:03 +0100

    reorganized procect to have all lambdas in one place

--- Data/YahooFinance/setup/install-browser.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Downloading Chromium..."
-curl "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1203200%2Fchrome-linux.zip?generation=1695993725821995&alt=media" > /tmp/chromium.zip
-unzip /tmp/chromium.zip -d /tmp/
-mv /tmp/chrome-linux/ /opt/chrome
-curl "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1203200%2Fchromedriver_linux64.zip?generation=1695993729397198&alt=media" > /tmp/chromedriver_linux64.zip
-unzip /tmp/chromedriver_linux64.zip -d /tmp/
-mv /tmp/chromedriver_linux64/chromedriver /opt/chromedriver
