a7667a7e1ca4d4577aacdc59f682a2a9233aa504
Lukasz Harazin <lukaszharazin@gmail.com>
2024-02-12 22:09:04 +0100

    calculated synthetic ETFs

---
+++ Data/AWS_Lambdas/sql/Insert_currency_rates_usd_symbols.sql
@@ -0,0 +1,10 @@
+INSERT INTO [dbo].[MarketSymbols] ([Code], [Description], [Category], [Country], [Source])
+VALUES ('HKDUSD=X', 'Hong Kong Dollar', 'Currency Rate USD', 'Hong Kong', 'Yahoo'),
+	('TWDUSD=X', 'New Taiwan Dollar', 'Currency Rate USD', 'Taiwan', 'Yahoo'),
+	('INRUSD=X', 'Indian Rupee', 'Currency Rate USD', 'India', 'Yahoo'),
+	('KRWUSD=X', 'South Korean Won', 'Currency Rate USD', 'Korea', 'Yahoo'),
+	('BRLUSD=X', 'Brazilian Real', 'Currency Rate USD', 'Brazil', 'Yahoo'),
+	('RUBUSD=X', 'Russian Ruble', 'Currency Rate USD', 'Russia', 'Yahoo'),	
+	('DKKUSD=X', 'Danish Krone', 'Currency Rate USD', 'Denmark', 'Yahoo'),
+	('NOKUSD=X', 'Norwegian Krone', 'Currency Rate USD', 'Norway', 'Yahoo'),
+	('ZARUSD=X', 'South African Rand', 'Currency Rate USD', 'South Africa', 'Yahoo')
