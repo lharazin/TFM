76c52bb827d3ba58453871eb60e592b9cdb0fa6d
Lukasz Harazin <lukaszharazin@gmail.com>
2024-05-06 22:41:27 +0200

    saved extra investing indicators

--- Data/AWS_Lambdas/sql/Insert_extra_Investing_indicators.sql
+++ Data/AWS_Lambdas/sql/Insert_extra_Investing_indicators.sql
@@ -1,4 +1,7 @@
 INSERT INTO [dbo].[EconomicIndicators] ([Indicator], [Category], [Source], [Frequency], [Measure])
 VALUES ('Producer Price Index', 'Prices', 'Investing', 'M', 'Growth YoY'),
 	('Industrial Production', 'Bussiness', 'Investing', 'M', 'Growth YoY'),
-	('Retail Sales', 'Consumer', 'Investing', 'M', 'Growth YoY')
+	('Retail Sales', 'Consumer', 'Investing', 'M', 'Growth YoY'),
+	('Producer Price Index (MoM)', 'Prices', 'Investing', 'M', 'Growth MoM'),
+	('Trade Balance', 'Trade', 'Investing', 'M', 'Local Currency'),
+	('Consumer Confidence ', 'Consumer', 'Investing', 'M', 'Index')
